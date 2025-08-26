0bbca078185859c883bbf061eab40f4dc8015273
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2025-08-26 17:36:06 +0900

    Made move boundaries of bunny

--- src/main.ts
+++ src/main.ts
@@ -14,11 +14,16 @@ import { Controller } from './controller';
   const texture = await Assets.load('/raw-assets/bunny.png');
   const bunny = new Sprite(texture);
 
-  bunny.anchor.set(0, 0);
   bunny.scale.set(0.2);
+  bunny.anchor.set(0, 0);
 
   bunny.x = 0;
   bunny.y = app.screen.height / 2;
+
+  function moveBunnyBy(bunny: Sprite, x: number, y: number) {
+    bunny.x = Math.min(Math.max(bunny.x + x, 0), app.screen.width - bunny.width);
+    bunny.y = Math.min(Math.max(bunny.y + y, 0), app.screen.height - bunny.height);
+  }
 
   // Game loop
 
@@ -28,21 +33,17 @@ import { Controller } from './controller';
     const shot = controller.keys.down.pressed;
 
     let direction = -1;
-    if (controller.keys.left.pressed) {
-      if (bunny.y < app.screen.left) return;
-      direction = -1;
-
-    }
+    if (controller.keys.left.pressed) direction = -1;
     else if (controller.keys.right.pressed) direction = 1;
-    else if (controller.keys.up.pressed) bunny.y -= 10;
-    else if (controller.keys.down.pressed) bunny.y += 10;
+    else if (controller.keys.up.pressed) moveBunnyBy(bunny, 0, -10);
+    else if (controller.keys.down.pressed) moveBunnyBy(bunny, 0, 10);
 
         // Determine the scene's horizontal scrolling speed based on the character's state.
     let speed = 5;
 
 
     // Shift the scene's position based on the character's facing direction, if in a movement state.
-    if (walk) bunny.x += speed * direction;
+    if (walk) moveBunnyBy(bunny, speed * direction, 0);
   })
 
   app.stage.addChild(bunny);
