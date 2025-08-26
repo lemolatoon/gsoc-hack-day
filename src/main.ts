0e0e68427f11569a459c1e86384a0c213a6d2b65
Soumyajyotii Ssarkar <soumyajyotisarkar23@gmail.com>
2025-08-26 13:54:57 +0530

    getting-up-and-down

--- src/main.ts
+++ src/main.ts
@@ -14,21 +14,28 @@ import { Controller } from './controller';
   const texture = await Assets.load('/raw-assets/bunny.png');
   const bunny = new Sprite(texture);
 
-  bunny.anchor.set(0.5, 0);
+  bunny.anchor.set(0, 0);
   bunny.scale.set(0.2);
 
-
-  bunny.x = app.screen.width / 2;
+  bunny.x = 0;
   bunny.y = app.screen.height / 2;
+
+  // Game loop
 
 
   app.ticker.add(() => {
     const walk = controller.keys.left.pressed || controller.keys.right.pressed;
     const shot = controller.keys.down.pressed;
+
     let direction = -1;
-    if (controller.keys.left.pressed) direction = -1;
+    if (controller.keys.left.pressed) {
+      if (bunny.y < app.screen.left) return;
+      direction = -1;
+
+    }
     else if (controller.keys.right.pressed) direction = 1;
-    const jump = controller.keys.space.pressed;
+    else if (controller.keys.up.pressed) bunny.y -= 10;
+    else if (controller.keys.down.pressed) bunny.y += 10;
 
         // Determine the scene's horizontal scrolling speed based on the character's state.
     let speed = 5;
